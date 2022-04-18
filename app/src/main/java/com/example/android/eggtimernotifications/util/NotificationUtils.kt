ec1078daac82cd896ebe282e9461f01ff082bb20
ILYAS KERBAL <kerbalsc@gmail.com>
2022-04-18 03:49:35 +0000

    Exercise: Add a Content Intent

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -41,9 +41,14 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // Create the content intent for the notification, which launches
     // this activity
     // TODO: Step 1.11 create intent
-
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
     // TODO: Step 1.12 create PendingIntent
-
+    val contentPendingIntent = PendingIntent.getActivities(
+        applicationContext,
+        NOTIFICATION_ID,
+        arrayOf(contentIntent),
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
     // TODO: Step 2.0 add style
 
     // TODO: Step 2.2 add snooze action
@@ -59,6 +64,9 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+
     // TODO: Step 1.13 set content intent
 
         // TODO: Step 2.1 add style to builder
